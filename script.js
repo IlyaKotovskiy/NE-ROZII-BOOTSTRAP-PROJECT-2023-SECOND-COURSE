c85fdc136edcfd33ce0a92ae3360ad407a1822a1
Ilya <ilundrik026@gmail.com>
2023-05-18 23:03:17 +0300

    header, bouquet, delivery, buy(fix bag with body)

---
+++ script.js
@@ -0,0 +1,55 @@
+const swiperFirst = new Swiper(".buy-swiper-first", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    allowTouchMove: false,
+    navigation: {
+        nextEl: "#swiper-button-next-first",
+        prevEl: "#swiper-button-prev-first",
+    },
+    autoplay: {
+        delay: 3000,
+        pauseOnMouseEnter: false
+    },
+});
+
+const swiperSecond = new Swiper(".buy-swiper-second", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    allowTouchMove: false,
+    navigation: {
+        nextEl: "#swiper-button-next-second",
+        prevEl: "#swiper-button-prev-second",
+    },
+    autoplay: {
+        delay: 3300,
+        pauseOnMouseEnter: false
+    },
+});
+
+const swiperThird = new Swiper(".buy-swiper-third", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    allowTouchMove: false,
+    navigation: {
+        nextEl: "#swiper-button-next-third",
+        prevEl: "#swiper-button-prev-third",
+    },
+    autoplay: {
+        delay: 3500,
+        pauseOnMouseEnter: false
+    },
+});
+
+const swiperFour = new Swiper(".buy-swiper-four", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    allowTouchMove: false,
+    navigation: {
+        nextEl: "#swiper-button-next-four",
+        prevEl: "#swiper-button-prev-four",
+    },
+    autoplay: {
+        delay: 3200,
+        pauseOnMouseEnter: false
+    },
+});
