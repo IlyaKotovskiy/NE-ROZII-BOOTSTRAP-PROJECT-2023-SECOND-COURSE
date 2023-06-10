90cddaa75e82590624e4d6494b8ebf567182fe7b
Ilya <ilundrik026@gmail.com>
2023-06-10 18:12:17 +0300

    finally

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const swiperFirst = new Swiper(".buy-swiper-first", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    allowTouchMove: false,
     navigation: {
         nextEl: "#swiper-button-next-first",
         prevEl: "#swiper-button-prev-first",
@@ -10,12 +7,28 @@ const swiperFirst = new Swiper(".buy-swiper-first", {
         delay: 3000,
         pauseOnMouseEnter: false
     },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+            allowTouchMove: false,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 2,
+            allowTouchMove: true,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 2,
+        },
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
+    }
 });
 
 const swiperSecond = new Swiper(".buy-swiper-second", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    allowTouchMove: false,
     navigation: {
         nextEl: "#swiper-button-next-second",
         prevEl: "#swiper-button-prev-second",
@@ -24,12 +37,28 @@ const swiperSecond = new Swiper(".buy-swiper-second", {
         delay: 3300,
         pauseOnMouseEnter: false
     },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+            allowTouchMove: false,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 2,
+            allowTouchMove: true,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 2,
+        },
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
+    }
 });
 
 const swiperThird = new Swiper(".buy-swiper-third", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    allowTouchMove: false,
     navigation: {
         nextEl: "#swiper-button-next-third",
         prevEl: "#swiper-button-prev-third",
@@ -38,12 +67,28 @@ const swiperThird = new Swiper(".buy-swiper-third", {
         delay: 3500,
         pauseOnMouseEnter: false
     },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+            allowTouchMove: false,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 2,
+            allowTouchMove: true,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 2,
+        },
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
+    }
 });
 
 const swiperFour = new Swiper(".buy-swiper-four", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    allowTouchMove: false,
     navigation: {
         nextEl: "#swiper-button-next-four",
         prevEl: "#swiper-button-prev-four",
@@ -52,4 +97,23 @@ const swiperFour = new Swiper(".buy-swiper-four", {
         delay: 3200,
         pauseOnMouseEnter: false
     },
+    breakpoints: {
+        992: {
+            slidesPerView: 3,
+            allowTouchMove: false,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 2,
+            allowTouchMove: true,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 2,
+        },
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
+    }
 });
